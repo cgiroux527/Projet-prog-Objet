e063ac82e96ec2d1f00a309ee316f59512808107
mingo <laforcebrute0@gmail.com>
2026-04-17 09:47:39 -0400

    create test

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mingo on 2026-04-17.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,13 @@
+//
+// Created by mingo on 2026-04-17.
+//
+
+#ifndef PROJET_PROG_OBJET_TEST_H
+#define PROJET_PROG_OBJET_TEST_H
+
+
+class test {
+};
+
+
+#endif //PROJET_PROG_OBJET_TEST_H
